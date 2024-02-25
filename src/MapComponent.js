7f59a03c182431d3cf2e2a6e1f126caa17a3be02
Kara Siegel <karasiegel@college.harvard.edu>
2024-02-25 18:30:35 -0500

    added pinning based on current and destination location, find distance between two locations and working on directions

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import React, { useState, useEffect } from "react";
 import {
-  setKey,
-  setDefaults,
-  setLanguage,
-  setRegion,
-  fromAddress,
-  fromLatLng,
-  fromPlaceId,
-  setLocationType,
-  geocode,
-  RequestType,
-} from "react-geocode";
-// const google = window.google;
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  DistanceMatrixService,
+  DirectionsService,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import { setDefaults, fromAddress } from "react-geocode";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const center = {
   lat: -3.745,
@@ -24,11 +22,6 @@ const containerStyle = {
   height: "400px",
 };
 
-// var markerPosition = {
-//   lat: -3.745,
-//   lng: -38.523,
-// };
-
 setDefaults({
   key: process.env.REACT_APP_GOOGLE_API_KEY, // Your API key here.
   language: "en", // Default language for responses.
@@ -36,18 +29,15 @@ setDefaults({
 });
 
 function MapComponent() {
-  const [coordinates, setCoordinates] = useState(null);
-  let marker;
-  let geocoder;
-  let responseDiv;
-  let response;
+  const [currentCoordinates, setCurrentCoordinates] = useState(null);
+  const [destinationCoordinates, setDestinationCoordinates] = useState(null);
+  const [myLocation, setMyLocation] = useState("");
+  const [destination, setDestination] = useState("");
 
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   });
-
-  //   geocoder = new google.maps.Geocoder();
 
   const [map, setMap] = React.useState(null);
 
@@ -63,10 +53,19 @@ function MapComponent() {
     setMap(null);
   }, []);
 
-  const findLatAndLng = () => {
-    fromAddress("Eiffel Tower").then(
+  const findLatAndLng = (location) => {
+    console.log(myLocation);
+    fromAddress(location).then(
       (response) => {
-        setCoordinates(response.results[0].geometry.location);
+        if (location === myLocation) {
+          setCurrentCoordinates(response.results[0].geometry.location);
+          console.log("success");
+          console.log(currentCoordinates);
+        } else {
+          setDestinationCoordinates(response.results[0].geometry.location);
+          console.log("success");
+          console.log(destinationCoordinates);
+        }
       },
       (error) => {
         console.error(error);
@@ -74,18 +73,101 @@ function MapComponent() {
     );
   };
 
-  findLatAndLng();
+  function handleSubmit() {
+    // e.preventDefault();
+    // console.log(e.target.value);
+    //setMyLocation(e.target.value)
+    console.log(myLocation);
+    if (myLocation) {
+      findLatAndLng(myLocation);
+    }
+    console.log(destination);
+    if (destination) {
+      findLatAndLng(destination);
+    }
+  }
+
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      <Marker position={coordinates} />
-      <></>
-    </GoogleMap>
+    <Box>
+      <Box component="form" noValidate sx={{ mt: 1 }}>
+        <TextField
+          id="outlined-basic"
+          label="Your Location"
+          variant="outlined"
+          value={myLocation}
+          onChange={(e) => setMyLocation(e.target.value)}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Destination"
+          variant="outlined"
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
+        />
+        <Button
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
+      </Box>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {/* <Marker position={coordinates} /> */}
+        {/* {console.log(coordinates)}
+        <Marker position={{ lat: coordinates?.lat, lng: coordinates?.lng }} /> */}
+        {currentCoordinates ? <Marker position={currentCoordinates} /> : <></>}
+        {destinationCoordinates ? (
+          <Marker position={destinationCoordinates} />
+        ) : (
+          <></>
+        )}
+        {/* <Marker position={{ lat: -3.745, lng: -38.523 }} /> */}
+        <></>
+        {currentCoordinates && destinationCoordinates ? (
+          <Box>
+            <DistanceMatrixService
+              options={{
+                destinations: [destinationCoordinates],
+                origins: [currentCoordinates],
+                travelMode: "WALKING",
+              }}
+              callback={(response) => {
+                console.log(response);
+                //Distance between two locations
+                console.log(response.rows[0].elements[0].distance.text);
+                // Time it takes to walk between two location
+                console.log(response.rows[0].elements[0].duration.text);
+              }}
+            />
+            {console.log("hi")}
+            <DirectionsService
+              options={{
+                destination: [destinationCoordinates],
+                origin: [currentCoordinates],
+                travelMode: "DRIVING",
+              }}
+              callback={(result, status) => {
+                console.log(result);
+                // this.setState({
+                //     directions: result
+                // });
+              }}
+            />
+            {console.log("bye")}
+          </Box>
+        ) : (
+          <></>
+        )}
+      </GoogleMap>
+    </Box>
   ) : (
     <></>
   );
